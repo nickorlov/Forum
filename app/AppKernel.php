d7fb6d411049bb0905f596b974d40b3172436f9f
nickorlov <neserag95@gmail.com>
2018-11-09 00:06:01 +0200

    Add Categories with hierarchy

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\UserBundle\FOSUserBundle(),
             new Knp\Bundle\MenuBundle\KnpMenuBundle(),
             new Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle(),
+            new Stof\DoctrineExtensionsBundle\StofDoctrineExtensionsBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
--- src/ForumBundle/Controller/DefaultController.php
+++ src/ForumBundle/Controller/DefaultController.php
@@ -13,8 +13,23 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $repo = $this->getDoctrine()->getRepository('ForumBundle:Category');
+        $options = array(
+            'decorate' => true,
+            'rootOpen' => '<ul>',
+            'rootClose' => '</ul>',
+            'childOpen' => '<li>',
+            'childClose' => '</li>'
+        );
+        $htmlTree = $repo->childrenHierarchy(
+            null, /* starting from root nodes */
+            false, /* true: load all children, false: only direct */
+            $options
+        );
+
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'tree' => $htmlTree,
         ]);
     }
 }
